f7bbaf6eb5575998aedecf6e32edf235b089b411
Malcolm Williams <mogwms@gmail.com>
2025-10-20 12:27:44 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("My name is John Doe! Say, it's a wonderful morning today, isn't it?");
+    }
+}
